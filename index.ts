7bb9b5875e17321fe73aeaa1292e9bf2a3f26dcc
Nassim Dridi <nassim.dridi@cognira.com>
2026-03-18 11:29:40 +0100

    Adding cache

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import express, { type Request, type Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import redisClient, { connectRedis } from './cache.js';
 
 
 const app = express();
@@ -12,11 +13,26 @@ const adapter = new PrismaPg({
   connectionString: process.env["DATABASE_URL"] || "postgresql://nassim:nassimpassword@localhost:5430/coffee_shop",
 });
 const prisma = new PrismaClient({ adapter });
+await connectRedis();
 
 // 1. Get All Customers (Type-Safe)
 app.get('/customers', async (req: Request, res: Response) => {
+    const cacheKey = 'all_customers';
+
   try {
+        // 1. Try to get data from Cache
+    const cachedData = await redisClient.get(cacheKey);
+    
+    if (cachedData) {
+      console.log("🚀 Serving from Cache!");
+      return res.json(JSON.parse(cachedData));
+    }
+    
     const result = await prisma.customer.findMany();
+    // 3. Save to Cache for 60 seconds
+    await redisClient.set(cacheKey, JSON.stringify(result), {
+      EX: 60 
+    });
     res.json(result);
   } catch (err) {
     console.error(err);
